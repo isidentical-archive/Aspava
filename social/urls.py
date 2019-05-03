f7d7953090574e30f45d300ab1d06b46b0e18142
Batuhan Taşkaya <btaskaya33@gmail.com>
2019-05-03 18:41:50 +0300

    Sign in, sign up and patternmanager

--- social/urls.py
+++ social/urls.py
@@ -1,3 +1,32 @@
-from django.urls import path
+from collections import UserList
+from django.urls import include, path
+from social.views import Home, Register
 
-urlpatterns = ()
+class PatternManager:
+    """No list / dict comp, no context manager
+    no abc, its the simplest way to do a PatternManager """
+    
+    def __init__(self):
+        self.data = []
+        patterns = dict(filter(lambda member: not member[0].startswith('_'), vars(self.__class__).items()))
+        
+        if 'includes' in patterns:
+            for route, module in patterns.pop('includes').items():
+                self.data.append(path(route, include(module)))
+                
+        for name, route in patterns.items():
+            route, view = route
+            if hasattr(view, 'as_view'):
+                view = view.as_view()
+                
+            pattern = path(route, view, name=name)
+            self.data.append(pattern)
+    
+        
+class SocialPatterns(PatternManager, UserList):
+    home = '', Home
+    register = 'accounts/register/', Register
+    
+    includes = {'accounts': 'django.contrib.auth.urls'}
+
+urlpatterns = SocialPatterns()
--- social/views.py
+++ social/views.py
@@ -1,1 +1,13 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.views.generic.base import TemplateView
 
+
+class Home(TemplateView):
+    template_name = 'home.html'
+
+class Register(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/register.html'
