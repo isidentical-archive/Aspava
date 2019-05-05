5d9dc30aba10e502ba07c57a02ef7ac4c125be0d
Batuhan Taşkaya <btaskaya33@gmail.com>
2019-05-05 10:33:55 +0300

    Syntax highlight, extended list view

--- social/views.py
+++ social/views.py
@@ -1,35 +1,26 @@
+from itertools import chain
+
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
 from django.views.generic.base import TemplateView
 from social.models import Post, Snippet
 
-from operator import or_
-from functools import wraps, reduce
-
-def ret_or_super(func):
-    def wrapper(*args, **kwargs):
-        try:
-            res = func(*args, **kwargs)
-        except:
-            self = args[0]
-            res = getattr(super(self.__class__, self), func.__name__)(*args, **kwargs)
-        
-        return res
-        
-    return wrapper
 
 class ExtendedListView(ListView):
     def get_queryset(self):
-        queryset = reduce(or_, map(lambda model: model._default_manager.all(), self.models))
-        return queryset
+        return chain.from_iterable(map(lambda model: model._default_manager.all(), self.models))
         
-    
 class Home(ExtendedListView):
     models = Post, Snippet
     template_name = 'home.html'
     context_object_name = 'feed'
     
+    def get_queryset(self):
+        qs = list(super().get_queryset())
+        qs.sort(key = (lambda item: item.pub_date))
+        return qs
+        
 class Register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
