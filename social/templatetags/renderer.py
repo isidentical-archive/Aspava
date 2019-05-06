8340ea46016b00df4ba6a2e7fa3e14c790c25055
Batuhan Taşkaya <btaskaya33@gmail.com>
2019-05-06 04:26:42 +0300

    Migrate to purima

--- social/templatetags/renderer.py
+++ social/templatetags/renderer.py
@@ -5,6 +5,7 @@ from urllib.request import urlopen
 
 from django import template
 from django.template.loader import render_to_string
+from purima.utils.get_preview import get_preview
 
 register = template.Library()
 
@@ -22,83 +23,4 @@ def as_html(item):
     return result
 
 
-class MetaParser(HTMLParser):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._meta = {}
-
-    def handle_starttag(self, tag, attrs):
-        attrs = dict(attrs)
-        if tag == "meta" and attrs.get("property", "").startswith("og:"):
-            self._meta[attrs["property"].strip("og:")] = attrs.get("content")
-
-
-class SimpleHTMLConstructor:
-    def __init__(self):
-        self._buffer = StringIO()
-        self._indent = 0
-
-    def nl(self):
-        self._buffer.write("\n")
-        self._buffer.write(" " * 4 * self._indent)
-
-    def write(self, *text):
-        self._buffer.write("".join(text))
-        self.nl()
-
-    def ctx(self, *args, **kwds):
-        with self.tag(*args, **kwds):
-            pass
-
-    @contextmanager
-    def tag(self, tag, close=True, **attrs):
-        attrs = self._strip_attrs(attrs)
-        try:
-            self._buffer.write(f"<{tag}")
-            for attr, value in attrs.items():
-                self._buffer.write(f" {attr}='{value}'")
-            self._buffer.write(">")
-            self._indent += 1
-            self.nl()
-            yield
-        finally:
-            self._indent -= 1
-            self.nl()
-            if close:
-                self._buffer.write(f"</{tag}>")
-
-    @staticmethod
-    def _strip_attrs(attrs):
-        if "cls" in attrs:
-            attrs["class"] = attrs.pop("cls")
-        return attrs
-
-    def __str__(self):
-        return self._buffer.getvalue()
-
-
-def construct_preview(meta):
-    html = SimpleHTMLConstructor()
-    with html.tag("div", cls="row"):
-        with html.tag("div", cls="col-3"):
-            html.ctx("img", close=False, src=meta["image"], alt=meta.get("title"))
-        with html.tag("div", cls="col-9"):
-            if meta.get("title"):
-                with html.tag("h3"):
-                    html.write(meta["title"], " ", meta.get("site_name"))
-                    html.ctx("br", close=False)
-            if meta.get("description"):
-                with html.tag("p"):
-                    html.write(meta["description"])
-
-    return html
-
-
-@register.simple_tag
-def get_preview(url):
-    parser = MetaParser()
-    with urlopen(url) as conn:
-        headers = conn.info()
-        parser.feed(conn.read().decode())
-
-    return construct_preview(parser._meta)
+register.simple_tag(get_preview)
--- social/urls.py
+++ social/urls.py
@@ -3,87 +3,12 @@ from dataclasses import dataclass
 from typing import Optional, Sequence
 
 from django.urls import include, path
+from purima.urls import PatternManager, IncludeFilter
 
 from social.views import Home, Profile, Register
 
 
-@dataclass(frozen=True, unsafe_hash=True)
-class IncludeFilter:
-    module: str
-    whitelist: Optional[Sequence] = None
-    blacklist: Optional[Sequence] = None
-
-    def __str__(self):
-        return self.module
-
-
-class PatternManager:
-    """A pattern manager for urlpatterns. 
-    
-    Subclass it with collections.UserList and then
-    add class variables about your routes.
-    
-        class XXXPatterns(PatternManager, UserList):
-            <name> = <path>, <view>
-        
-        urlpatterns = XXXPatterns()
-    
-    You dont need to call as_view() for generics. PatternManager
-    will call them for you. 
-    
-    If you need to include urls from another module define a class variable
-    called includes (it is reserved for this purpose, you cant use that as a <name>
-    for normal route). 
-    
-        includes = {<path>: <include>, 
-                    <path.n>: <include.n>, 
-                    'accounts': 'django.contrib.auth.urls'}
-    """
-
-    def __init__(self):
-        self.data = []
-        patterns = dict(
-            filter(
-                lambda member: not member[0].startswith("_"),
-                vars(self.__class__).items(),
-            )
-        )
-
-        if "includes" in patterns:
-            for route, module in patterns.pop("includes").items():
-                included = include(str(module))
-                if isinstance(module, IncludeFilter):
-                    included = self._filtered_include(module, included)
-
-                self.data.append(path(route, included))
-
-        for name, route in patterns.items():
-            route, view = route
-            if hasattr(view, "as_view"):
-                view = view.as_view()
-
-            pattern = path(route, view, name=name)
-            self.data.append(pattern)
-
-    def _filtered_include(self, module, included):
-        urlconf_module, *meta = included
-        urlpatterns = urlconf_module.urlpatterns
-        _type = type(urlpatterns)
-        if module.blacklist:
-            urlpatterns = filter(
-                lambda pattern: pattern.name not in module.blacklist, urlpatterns
-            )
-        if module.whitelist:
-            urlpatterns = filter(
-                lambda pattern: pattern.name in module.whitelist, urlpatterns
-            )
-
-        urlconf_module.urlpatterns = _type(urlpatterns)
-        included = urlconf_module, *meta
-        return included
-
-
-class SocialPatterns(PatternManager, UserList):
+class SocialPatterns(PatternManager):
     home = "", Home
     profile = "people/<slug>/", Profile
     register = "accounts/register/", Register
@@ -94,5 +19,5 @@ class SocialPatterns(PatternManager, UserList):
         )
     }
 
-
 urlpatterns = SocialPatterns()
+
